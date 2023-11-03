61018ef124e8e57e1cffa2a60398bb8ed3150369
Bob Guo <bobguo1234@gmail.com>
2023-11-04 01:31:48 +1100

    Your new commit message here

--- generatehtml.py
+++ generatehtml.py
@@ -6,28 +6,38 @@ root_directory = 'data'
 # Get a list of subfolders
 subfolders = [f for f in os.listdir(root_directory) if os.path.isdir(os.path.join(root_directory, f))]
 
-# Create the index.html file
+
+
+html_content = "<html><head>"
+html_content += "<title>Image Gallery</title>"
+html_content += "<link rel='stylesheet' type='text/css' href='styles.css'>"  # Link to the external CSS file
+html_content += "</head><body>\n"
+
+# Create a container to hold the images
+html_content += "<div style='display: flex;'>\n"
+
+# Iterate through each subfolder
+for subfolder in subfolders:
+    # Get a list of images in the subfolder
+    image_files = [f for f in os.listdir(os.path.join(root_directory, subfolder)) if f.lower().endswith(('.jpg', '.jpeg', '.png', '.gif'))]
+
+    # Check if there are any images in the subfolder
+    if image_files:
+        # Get the path of the first image in the subfolder
+        first_image_path = os.path.join('data', subfolder, image_files[0])
+        # Create a link with the image as the thumbnail and apply the CSS class
+        html_content += f"<a href='{subfolder}.html'><img src='{first_image_path}' alt='{subfolder}' class='thumbnail-button'></a>\n"
+
+# Close the container
+html_content += "</div>\n"
+html_content += "</body></html>"
+
+# Write the HTML content to the file
 with open('index.html', 'w') as index_file:
-    index_file.write("<html><head><title>Image Gallery</title></head><body>\n")
+    index_file.write(html_content)
 
-    # Create a container to hold the images
-    index_file.write("<div style='display: flex;'>\n")
 
-    # Iterate through each subfolder
-    for subfolder in subfolders:
-        # Get a list of images in the subfolder
-        image_files = [f for f in os.listdir(os.path.join(root_directory, subfolder)) if f.lower().endswith(('.jpg', '.jpeg', '.png', '.gif'))]
 
-        # Check if there are any images in the subfolder
-        if image_files:
-            # Get the path of the first image in the subfolder
-            first_image_path = os.path.join('data', subfolder, image_files[0])
-            # Create a link with the image as the thumbnail
-            index_file.write(f"<a href='{subfolder}.html'><img src='{first_image_path}' alt='{subfolder}'></a>\n")
-    
-    # Close the container
-    index_file.write("</div>\n")
-    index_file.write("</body></html>\n")
 print("index.html created.")
 
 # Iterate through each subfolder and create an HTML page
@@ -36,6 +46,7 @@ for subfolder in subfolders:
     html_filename = f"{subfolder}.html"
     with open(html_filename, 'w') as html_file:
         # Write the HTML header
+        html_file.write(html_content)
         html_file.write("<html><head><title>Images</title></head><body>\n")
 
         # Get a list of images in the subfolder
